148208c60b33ba75fb16222b177c6f27cffb54e8
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-05 09:49:06 -0400

    Initial Creation
    Created a Home controller with login and sign up views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csc517p1::Application.config.secret_token = '93c275cb55749c7b3cac1bc77abb6dd52d244c7a708c9c1c6921d1c8356f61c8e6c30f67356acd7e2baa5a28facefd08ae32356ad084bf4dc3b9b35d546d7330'

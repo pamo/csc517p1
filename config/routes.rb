438ba4260cc1ffbc810ac40cb9625b6cd6be0ab8
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 17:05:25 -0400

    Added a home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Csc517p1::Application.routes.draw do
+  get "home/index"
+
   resources :users
 
   resources :posts
@@ -52,7 +54,7 @@ Csc517p1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 

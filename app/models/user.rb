27a5e11bd179be2eb677412bb7fcb81e41af8ae9
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 19:59:05 -0400

    added user authentication to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ActiveRecord::Base
-  attr_accessible :admin, :name, :password
+  validates :name, :presence => true, :uniqueness => true
+
+  validates :password, :confirmation => true
+  attr_accessor :password_confirmation
+  attr_reader   :password
+
+  validate  :password_must_be_present
+
+  def User.authenticate(name, password)
+    if user = find_by_name(name)
+        user
+    end
+  end
+
+  # 'password' is a virtual attribute
+  def password=(password)
+    @password = password
+  end
+
+  private
+
+  def password_must_be_present
+    errors.add(:password, "Missing password")
+  end
+
 end

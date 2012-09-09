605ff4ad9ff76ccb54a05a71eaf84727b2247ed1
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 19:04:59 -0400

    login redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,13 @@ class ApplicationController < ActionController::Base
   before_filter :authorize
   protect_from_forgery
 
+  private
+
   protected
-    def authorize
-      unless User.find_by_id(session[:user_id])
-        redirect_to login_url, :notice => "Please log in"
-      end
+
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, :notice => "Please log in"
     end
+  end
 end

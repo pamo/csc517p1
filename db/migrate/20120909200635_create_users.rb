01dd4758f76a8a6de840a6ad16c8c2c0d7a29885
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 19:16:04 -0400

    Revert "changed type to a boolean"
    This reverts commit 594e7b378b73be8a678fb48b0e60132884f81a03.

--- db/migrate/20120909200635_create_users.rb
+++ db/migrate/20120909200635_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.boolean :type
+      t.string :username
+      t.string :usertype
       t.string :password
 
       t.timestamps

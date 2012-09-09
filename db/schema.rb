85551c9442861ace876878b99cc09fe4283e9ba5
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 19:57:44 -0400

    new db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120909200635) do
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.boolean  "admin"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20120909235501) do
 
   create_table "posts", :force => true do |t|
     t.string   "username"
@@ -30,6 +22,12 @@ ActiveRecord::Schema.define(:version => 20120909200635) do
     t.datetime "updated_at", :null => false
   end
 
-
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password"
+    t.boolean  "admin"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end

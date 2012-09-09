a8f093ab9cbaaf30f43cb0fcbd0813a6917981d8
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 16:46:18 -0400

    Added workspace xml to gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.create([{username: 'admin', usertype: 'admin', password:'admin'},
+                    {username: 'Pam', usertype: 'default', password:'banana'}])
+

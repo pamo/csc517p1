e2c0c3326c90abd4f42f2181882f8b4d728dfdf1
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 19:26:17 -0400

    removed user creation from db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-users = User.create([{username: 'admin', usertype: 'admin', password:'admin'},
-                    {username: 'Pam', usertype: 'default', password:'banana'}])
 

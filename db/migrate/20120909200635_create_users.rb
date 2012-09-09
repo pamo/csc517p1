892e893d9aa950c8e5698ffa4c0cf6e045b6005c
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 19:22:35 -0400

    changed usertype to boolean

--- db/migrate/20120909200635_create_users.rb
+++ db/migrate/20120909200635_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :usertype
+      t.boolean :usertype
       t.string :password
 
       t.timestamps

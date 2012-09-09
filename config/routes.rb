79437397bced68b4b62ea055faa008476dfb5509
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-09 18:55:48 -0400

    colon typo

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,7 @@ Csc517p1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index', as: => 'home'
+   root :to => 'home#index', :as => 'home'
 
   # See how all your routes lay out with "rake routes"
 

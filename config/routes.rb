1fc7ae9171b33da9c13853939795ab0956d7348e
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-13 19:37:54 -0400

    messing around

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Csc517p1::Application.routes.draw do
-  resources :comments
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
   root :to => 'home#index', :as => 'home'
@@ -13,14 +11,18 @@ Csc517p1::Application.routes.draw do
 
   resources :users
   resources :posts
+  resources :comments
 
   get 'posts' => 'posts#index'
   get 'posts/index'
   get 'posts/show'
   get 'posts/new'
-
-
   match 'posts/search',   :to => 'posts#search'
+
+  get 'comments' => 'comments#index'
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/new'
 
   get 'admin' => 'admin#index'
 

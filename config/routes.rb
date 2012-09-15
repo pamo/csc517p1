1a73aa57f9010f29e9962a93cd8b7d3933b3fbc2
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-15 16:07:01 -0400

    removed some redundancies

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,13 @@ Csc517p1::Application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  resources :users
   resources :posts
   resources :comments
+  resources :users, :users => {:search => :get}
+
 
   get 'posts' => 'posts#index'
 
-  resources :users do
-    resources :posts, :only => [:new, :create, :show, :index, :destroy]
-  end
-
-  resources :users, :users => {:search => :get}
   # resources :posts, :posts => {:search => :get}
 
   match 'users/search',  :to => 'users#search'

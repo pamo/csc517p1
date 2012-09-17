ea8c8187c6603bf0a08665b9f1b9ae74b81d886d
Brittany Johnson <BJohnson2011@gmail.com>
2012-09-17 00:24:15 -0400

    added search into post and comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,13 @@ class Comment < ActiveRecord::Base
   #attr_reader :body, :post_id, :username, :votes
   #attr_writer :body, :post_id, :username, :votes
   belongs_to :post
+
+  def self.search(search)
+    if search
+      where('body LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
+
 end

d7334270e177977c0051396964f7060ad859f65c
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-16 23:13:16 -0400

    Added some helper methods to get the current user and to see if they are logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   before_filter :authorize
-  protect_from_forgery
 
-  private
+  helper_method :current_user, :logged_in?
 
-  protected
+  def logged_in?
+    current_user != nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 
   def authorize
-    unless User.find_by_id(session[:user_id])
+    unless logged_in?
       redirect_to login_url, :notice => "Please log in"
     end
   end

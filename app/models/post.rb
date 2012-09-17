f356065437c123ac0b1219c822aa7bd5f50adc92
Brittany Johnson <BJohnson2011@gmail.com>
2012-09-17 00:25:31 -0400

    Revert "added search into post and comment model"
    This reverts commit ea8c8187c6603bf0a08665b9f1b9ae74b81d886d.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,12 +8,4 @@ class Post < ActiveRecord::Base
 
   attr_accessible :category_id, :content, :username, :votes
 
-  def self.search(search)
-    if search
-      where('content LIKE ?', "%#{search}%")
-    else
-      scoped
-    end
-  end
-
 end

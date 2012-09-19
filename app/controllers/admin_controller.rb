46192193f800bd1b883738ed17ed77dfbd084bf4
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-19 10:53:47 -0400

    Admin reporting

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,24 +1,32 @@
 class AdminController < ApplicationController
   skip_before_filter :authorize
+  helper_method :post_votes, :comment_votes
 
   # GET /admin
   def index
     @users = User.all()
-    @posts = Post.all()
-    @comments = Comment.all()
-    @post_count = 0
-    @post_comment_count = 0
-    @post_vote_count = 0
-    @count_admin_posts = 0
-    @count_admin_comments = 0
-    @admin_post_comment_count = 0
-    @admin_post_vote_count = 0
-    @admin_comment_vote_count = 0
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
 
     end
+  end
+
+  def post_votes(user)
+    @post_votes = 0
+    Post.find_all_by_username(user).each do |p|
+      puts "post_votes"
+      @post_votes += Vote.count(:all, :conditions => {"pid" => p.id} )
+    end
+    return @post_votes
+  end
+
+  def comment_votes(user)
+    @comment_votes = 0
+    Comment.find_all_by_username(user).each do |c|
+      @comment_votes += Vote.count(:all, :conditions => {"cid" => c.id} )
+    end
+    return @comment_votes
   end
 end

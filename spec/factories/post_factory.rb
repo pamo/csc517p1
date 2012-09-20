e65419baae3ed52bcb843792cb6547afb990d651
Brittany Johnson <BJohnson2011@gmail.com>
2012-09-19 21:29:51 -0400

    comment/vote and post/vote relationships

--- spec/factories/post_factory.rb
+++ spec/factories/post_factory.rb
@@ -3,7 +3,17 @@ Factory.define do
     user
     content   "Hello World"
     category_id  1
-    votes  0
+    votes
+
+    factory :posts_with_votes do
+      ignore do
+        votes_count  0
+      end
+
+      after(:create) do |post, evaluator|
+        FactoryGirl.create_list(:votes, evaluator.votes_count, post: post)
+      end
+    end
   end
 
   factory :user do

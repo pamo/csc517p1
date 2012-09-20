6eaaf307a5eaebdb5e7785e9dc230ea0e3aa1f17
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-19 23:37:55 -0400

    User Model spec complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,17 @@ describe User do
   it "should require a username" do
     FactoryGirl.build(:user, :name => "").should_not be_valid
   end
+
+  it "should require a password" do
+    FactoryGirl.build(:user, :password =>"").should_not be_valid
+  end
+
+  it "should require matching passwords" do
+    FactoryGirl.build(:user, :password =>"aaa", :password_confirmation => "bbb").should_not be_valid
+  end
+
+  it "should require a unique username" do
+    FactoryGirl.create(:user)
+    FactoryGirl.build(:user).should_not be_valid
+  end
 end

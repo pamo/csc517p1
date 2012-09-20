2fa27950f1ccdb1ed0f8596fac6ecddf50e3ae38
Brittany Johnson <BJohnson2011@gmail.com>
2012-09-19 20:37:28 -0400

    functionality to create users in factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,15 +1,25 @@
+
+
 # This will guess the User class
+
 FactoryGirl.define do
   factory :user do
-    first_name "John"
-    last_name  "Doe"
+    user_name "JohnDoe"
+    password  "password"
+    password_confirmation "password"
     admin false
   end
 
   # This will use the User class (Admin would have been guessed)
   factory :admin, class: User do
-    first_name "Admin"
-    last_name  "User"
+    user_name "Admin"
+    password  "admin"
+    password_confirmation  "admin"
     admin      true
   end
 end
+
+built_users   = FactoryGirl.build_list(:user, 25)
+created_users = FactoryGirl.create_list(:user, 25)
+built_admins = FactoryGirl.build_list(:admin, 10)
+created_admins = FactoryGirl.create_list(:admin, 10)

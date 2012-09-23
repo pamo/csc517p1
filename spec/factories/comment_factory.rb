afc114fa0da2ec7770a0648ddc148a4abd31361b
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-23 10:50:14 -0400

    Comment model testing

--- spec/factories/comment_factory.rb
+++ spec/factories/comment_factory.rb
@@ -1,19 +1,19 @@
 FactoryGirl.define do
   factory :comment do
-    user
-    body   "Goodbye World"
-    votes
-    post
+    username    "Admin"
+    body        "Goodbye World"
+    votes       0
+    post_id     1
 
-    factory :comment_with_votes do
-      ignore do
-        votes_count  0
-      end
-
-      after(:create) do |comment, evaluator|
-        FactoryGirl.create_list(:votes, evaluator.comments_count, post: post)
-      end
-    end
+    #factory :comment_with_votes do
+    #  ignore do
+    #    votes_count  0
+    #  end
+    #
+    #  after(:create) do |comment, evaluator|
+    #    FactoryGirl.create_list(:votes, evaluator.comments_count, post: post)
+    #  end
+    #end
 
   end
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should create a new comment" do
+    FactoryGirl.build(:comment).should be_valid
+  end
+
+  it "should update a post" do
+    FactoryGirl.create(:comment, :body => "AAAAA").should be_valid
+  end
+
+  it "should require a username" do
+    FactoryGirl.build(:comment, :username => "").should_not be_valid
+  end
+
+  it "should require body" do
+    FactoryGirl.build(:comment, :body => "").should_not be_valid
+  end
+
+  it "should be associated with a post" do
+    FactoryGirl.build(:comment, :post_id => "").should_not be_valid
+  end
+
 end

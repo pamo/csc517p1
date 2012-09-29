a2aaeb5afde708a57c161a22271e21a34f772638
Brittany Johnson <BJohnson2011@gmail.com>
2012-09-29 16:08:11 -0400

    more test changes

--- spec/support/spec_test_helper.rb
+++ spec/support/spec_test_helper.rb
@@ -4,9 +4,13 @@ module SpecTestHelper
   end
 
   def login(user)
+=begin
     puts "spec_test_helper login(#{user.name})"
     #session[:user_id] = user.id
     session "/sessions/create", :username => user.name, :password => user.password
+=end
+    user = User.where(:username => user.to_s).first if user.is_a?(Symbol)
+    request.session[:user] = user.id
   end
 
 =begin

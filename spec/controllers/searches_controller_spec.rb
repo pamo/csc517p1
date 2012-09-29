a1f1a1ca1acc3bae85bb9b29f68385c9a0f83f4a
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-09-29 17:28:52 -0400

    properly rendering some views

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -4,13 +4,31 @@ require_relative '../spec_helper'
 describe SearchesController do
   describe "GET Index" do
     it "gets the index view" do
-      get "index"
+      get :index
       response.status.should be 200
     end
 
     it "gets the correct index view template" do
-      get "index"
-      response.should render_template("searches/index")
+      get :index
+      response.should render_template(:index)
     end
   end
+
+  describe "GET New" do
+    it "gets the new view" do
+      get :new
+      response.should render_template(:new)
+    end
+  end
+
+=begin
+  describe "GET Results" do
+    it "gets the results view" do
+      get :new
+      assign(:search, "blog")
+      render
+      response.should render_template(:index)
+    end
+  end
+=end
 end

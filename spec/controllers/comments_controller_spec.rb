7de1b11bd48e64cfd115060d0d68f4cb5adc152c
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-10-01 16:01:47 -0400

    re-commit conflict resolution

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -21,21 +21,6 @@ describe CommentsController do
       end
 
     end
-
-<<<<<<< HEAD
-    describe "when a user is logged in" do
-      it "should allow user to add comment" do
-        login(@user)
-        post :create, :id => "1", :comment => {}
-        #response.should have_tag("form#new_comment")
-        #response.should redirect_to(post_path(:post_id))
-        flash[:notice].should_not be_nil
-
-      end
-
-    end
-=======
->>>>>>> Removed test because we're going to test in posts controller instead
   end
 
 end

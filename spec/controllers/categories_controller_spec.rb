61aae2e32f08ef50ef7da1791f507f42e1634a88
Pamela Ocampo <pamela.ocampo@gmail.com>
2012-10-01 17:07:43 -0400

    tested everything but search action

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,16 +1,53 @@
-require_relative '../spec_helper'
+require 'spec_helper'
+require_relative '../../spec/support/spec_test_helper'
 
 
 describe CategoriesController do
+  render_views
   describe "GET Index" do
     it "gets the index view" do
-      get "index"
+      @user = FactoryGirl.build(:user)
+      login(@user)
+      get :index
       response.status.should be 200
-    end
-
-    it "gets the correct index view template" do
-      get "index"
       response.should render_template("categories/index")
     end
+  end
+  describe "Create Category" do
+
+    it "gets the new view" do
+      @category = FactoryGirl.build(:category)
+      @admin = FactoryGirl.build(:user, name: "admin")
+      login(@admin)
+      get :new
+      response.should render_template(:new)
+    end
+
+    it "creates a new category" do
+      @category = FactoryGirl.build(:category)
+      @admin = FactoryGirl.build(:user, name: "admin")
+      login(@admin)
+      put :create, :name => @category.name
+      response.should render_template(@category)
+
+    end
+
+    it "updates a category" do
+      @category = FactoryGirl.create(:category)
+      @admin = FactoryGirl.build(:user, name: "admin")
+      login(@admin)
+      put :update, { :id => @category.id, :category => { :name => "" } }
+      response.should render_template(:edit)
+
+    end
+
+    it "destroys a category" do
+      @category = FactoryGirl.create(:category)
+      @admin = FactoryGirl.build(:user, name: "admin")
+      login(@admin)
+      put :destroy, { :id => @category.id }
+      response.should redirect_to(categories_url)
+    end
+
   end
 end
